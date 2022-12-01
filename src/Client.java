d364da8dffd5b785051ea2c2b5ef3a45007a6caf
Halil Bülke <tokibokit@gmail.com>
2022-12-01 20:08:45 +0300

    asd

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,6 @@
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
@@ -29,13 +31,23 @@ public class Client {
 //                System.out.format("%-20s %s\n", store, store.getAttribute("volume:vsn"));
 //            }
             String command = "wmic baseboard get serialnumber";
-            String MBSerialNumber;
-            Process SerialNumberProcess = Runtime.getRuntime().exec(command);
-            InputStreamReader ISR = new InputStreamReader(SerialNumberProcess.getInputStream());
-            BufferedReader br = new BufferedReader(ISR);
-            MBSerialNumber = br.readLine();
-            SerialNumberProcess.waitFor();
-            br.close();
+            String MBSerialNumber=new String();
+            File file = File.createTempFile("realhowto", ".vbs");
+            file.deleteOnExit();
+            FileWriter fw = new java.io.FileWriter(file);
+            String vbs = "Set objWMIService = GetObject(\"winmgmts:\\\\.\\root\\cimv2\")\n"
+                    + "Set colItems = objWMIService.ExecQuery _ \n" + "   (\"Select * from Win32_BaseBoard\") \n"
+                    + "For Each objItem in colItems \n" + "    Wscript.StdOut.Writeline objItem.SerialNumber \n"
+                    + "    exit for  ' do the first cpu only! \n" + "Next \n";
+            fw.write(vbs);
+            fw.close();
+            Process p = Runtime.getRuntime().exec("cscript //NoLogo " + file.getPath());
+            BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line = new String();
+            while ((line = input.readLine()) != null) {
+                MBSerialNumber += line;
+            }
+            input.close();
             System.out.println("My Motherboard ID: " + MBSerialNumber);
             this.rawText = MessageFormat.format("{0}${1}${2}${3}${4}", username,userSerialNumber,macAddress,
                     diskID, MBSerialNumber);
