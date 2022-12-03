e45019fb7779d05b2ef6f28e8b8e87d8d4413e73
theroxz <ahmet.eren4219@hotmail.com>
2022-12-03 15:40:13 +0300

    ödev bitti

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,4 @@
+import javax.crypto.Cipher;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
@@ -6,12 +7,21 @@ import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.nio.file.FileStore;
 import java.nio.file.FileSystems;
+import java.security.MessageDigest;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
 import java.text.MessageFormat;
+import java.util.Base64;
 
 public class Client {
     public String rawText;
-    public Client(){
+    public byte[] encryptedBytes;
+    public byte[] hashValue;
+    public PublicKey pubKey;
+    public Client(PublicKey pubKey){
         try {
+            this.pubKey = pubKey;
             String username = "whoami";
             String userSerialNumber ="3052-2512-5690";
             System.out.println("Client started...");
@@ -55,5 +65,23 @@ public class Client {
         catch (Exception e){
             e.printStackTrace();
         }
+    }
+    public void beginProcess() throws Exception{
+        byte[] bytesToEncrypt = this.rawText.getBytes();
+        Cipher encryptCipher = Cipher.getInstance("RSA");
+        encryptCipher.init(Cipher.ENCRYPT_MODE, this.pubKey);
+        this.encryptedBytes=encryptCipher.doFinal(bytesToEncrypt);
+        MessageDigest digest = MessageDigest.getInstance("MD5");
+        this.hashValue = digest.digest(this.rawText.getBytes());
+    }
+    public void printMessages(boolean printEncrypted){
+        System.out.print("Client -- Raw License Text: ");
+        System.out.println(this.rawText);
+        if (printEncrypted) {
+            System.out.print("Client -- Encrypted License Text: ");
+            System.out.println(Base64.getEncoder().encodeToString(this.encryptedBytes));
+        }
+        System.out.print("Client -- MD5 License Text: ");
+        System.out.println(new String(this.hashValue));
     }
 }
--- src/LicenseManager.java
+++ src/LicenseManager.java
@@ -1,8 +1,94 @@
+import javax.crypto.Cipher;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
 public class LicenseManager {
+    public Signature sr;
     public Client c;
+    PrivateKey priKey;
+    PublicKey pubKey;
     public LicenseManager(){
-        c = new Client();
+        try {
+            readKeyFiles();
+            this.sr = Signature.getInstance("SHA256WithRSA");
+        }
+        catch (Exception e){}
+        c = new Client(pubKey);
         System.out.println(c.rawText);
         System.out.println("License Manager Service started...");
+        this.verifyLicense();
+    }
+
+    public void readKeyFiles() throws NoSuchAlgorithmException, InvalidKeySpecException, IOException {
+        byte[] priKeyBytes = Files.readAllBytes(Paths.get("private.key"));
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(priKeyBytes);
+        this.priKey = keyFactory.generatePrivate(keySpec);
+        byte[] keyBytes = Files.readAllBytes(Paths.get("public.key"));
+        X509EncodedKeySpec spec =
+                new X509EncodedKeySpec(keyBytes);
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        this.pubKey = kf.generatePublic(spec);
+    }
+    public void verifyLicense(){
+        try {
+            //license.txt yoksa süreci başlat. direkt servera request at signature işini yap.
+            //license.txt varsa süreci başlat. digital signatureü public key ile decrypt et kıyas doğruysa succeed de.
+            //kıyas yanlışsa fail de servera request at signature işini yap.
+            c.beginProcess();
+            byte[] digitalSignature = Files.readAllBytes(Paths.get("license.txt"));
+            c.printMessages(false);
+            System.out.print("Client -- Digital Signature: ");
+            System.out.println(Base64.getEncoder().encodeToString(digitalSignature));
+            sr.initVerify(c.pubKey);
+            sr.update(c.hashValue);
+            boolean verified = sr.verify(digitalSignature);
+            if (!verified) throw new SignatureException();
+            System.out.println("Client -- Succeed. The license is correct.");
+
+        }
+        catch (NoSuchFileException e){
+            System.out.println("Client -- License file is not found.");
+            c.printMessages(true);
+            this.signLicense();
+        }
+        catch (SignatureException e){
+            System.out.println("Client -- The license file has been broken!!");
+            this.signLicense();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+        public void signLicense(){
+            try {
+                System.out.println("Server -- Server is being requested.");
+                System.out.print("Server -- Incoming Encrypted Text: ");
+                System.out.println(Base64.getEncoder().encodeToString(c.encryptedBytes));
+                Cipher decryptCipher = Cipher.getInstance("RSA");
+                decryptCipher.init(Cipher.DECRYPT_MODE, this.priKey);
+                byte[] decryptedBytes = decryptCipher.doFinal(c.encryptedBytes);
+                System.out.print("Server -- Decrypted Text: ");
+                System.out.println(new String(decryptedBytes));
+                System.out.print("Server -- MD5 Plain License Text: ");
+                System.out.println(new String(c.hashValue));
+                sr.initSign(priKey);
+                sr.update(c.hashValue);
+                System.out.print("Server -- Digital Signature: ");
+                byte[] signedBytes = sr.sign();
+                System.out.println(Base64.getEncoder().encodeToString(signedBytes));
+                Files.write(Paths.get("license.txt"), signedBytes);
+                System.out.println("Client -- Succeed. The license file content is secured and signed by the server.");
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
     }
 }
